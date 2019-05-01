31705bab5041e36a4e580dd54a28471ce6061ef6
gorani4545 <gorani4545@gmail.com>
2019-05-01 17:59:00 +0900

    05:58

--- src/main/java/com/once/web/controller/KthController.java
+++ src/main/java/com/once/web/controller/KthController.java
@@ -1,9 +1,7 @@
 package com.once.web.controller;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import org.jsoup.Jsoup;
@@ -14,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
