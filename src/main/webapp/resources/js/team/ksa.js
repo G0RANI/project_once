30cd607270de7b90b9499281fb5d7c0d334a8c9e
gorani4545 <gorani4545@gmail.com>
2019-05-01 20:47:14 +0900

    21:00

--- src/main/webapp/resources/js/team/ksa.js
+++ src/main/webapp/resources/js/team/ksa.js
@@ -23,9 +23,6 @@ ksa=(()=>{
 				 $(ksa_compo.d_w_charge()).appendTo('#once');
 				 $('#nav2 a').attr('class', '');
 				 $('#ksa_d').attr('class', 'on');
-				 $('#port').click(()=>{
-					 port();
-				 });
 			});
 			break;
 		case 'i':
@@ -93,38 +90,67 @@ ksa=(()=>{
 	};
 	let port=()=>{
 		var IMP = window.IMP; // 생략가능
-		IMP.init('imp75272689');
-		// 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
-		// i'mport 관리자 페이지 -> 내정보 -> 가맹점식별코드
-		IMP.request_pay({ // param
-		    pg: "inicis",
-		    pay_method: "card",
-		    merchant_uid: "ORD20180131-0000011",
-		    name: "노르웨이 회전 의자",
-		    amount: 64900,
-		    buyer_email: "gildong@gmail.com",
-		    buyer_name: "홍길동",
-		    buyer_tel: "010-4242-4242",
-		    buyer_addr: "서울특별시 강남구 신사동",
-		    buyer_postcode: "01181"
-		}, function (rsp) { // callback
-		    if (rsp.success) { // 결제 성공 시: 결제 승인 또는 가상계좌 발급에 성공한 경우
-		        // jQuery로 HTTP 요청
-		        jQuery.ajax({
-		            url: "https://www.myservice.com/payments/complete", // 가맹점 서버
-		            method: "POST",
-		            headers: { "Content-Type": "application/json" },
-		            data: {
-		                imp_uid: rsp.imp_uid,
-		                merchant_uid: rsp.merchant_uid
-		            }
-		        }).done(function (data) {
-
-		        })
-		    } else {
-		        alert("결제에 실패하였습니다. 에러 내용: " +  rsp.error_msg);
-		    }
-		});
+        IMP.init('imp75272689'); 
+        // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
+        // i'mport 관리자 페이지 -> 내정보 -> 가맹점식별코드
+        IMP.request_pay({
+            pg: 'inicis', // version 1.1.0부터 지원.
+            /* 
+                'kakao':카카오페이, 
+                html5_inicis':이니시스(웹표준결제)
+                    'nice':나이스페이
+                    'jtnet':제이티넷
+                    'uplus':LG유플러스
+                    'danal':다날
+                    'payco':페이코
+                    'syrup':시럽페이
+                    'paypal':페이팔
+                */
+            pay_method: 'card',
+            /* 
+                'samsung':삼성페이, 
+                'card':신용카드, 
+                'trans':실시간계좌이체,
+                'vbank':가상계좌,
+                'phone':휴대폰소액결제 
+            */
+            merchant_uid: 'merchant_' + new Date().getTime(),
+            /* 
+                merchant_uid에 경우 
+                https://docs.iamport.kr/implementation/payment
+                위에 url에 따라가시면 넣을 수 있는 방법이 있습니다.
+                참고하세요. 
+                나중에 포스팅 해볼게요.
+             */
+            name: '주문명:결제테스트',
+            //결제창에서 보여질 이름
+            amount: 1000, 
+            //가격 
+            buyer_email: 'iamport@siot.do',
+            buyer_name: '구매자이름',
+            buyer_tel: '010-1234-5678',
+            buyer_addr: '서울특별시 강남구 삼성동',
+            buyer_postcode: '123-456',
+            m_redirect_url: 'https://www.yourdomain.com/payments/complete'
+            /*  
+                모바일 결제시,
+                결제가 끝나고 랜딩되는 URL을 지정 
+                (카카오페이, 페이코, 다날의 경우는 필요없음. PC와 마찬가지로 callback함수로 결과가 떨어짐) 
+                */
+        }, function (rsp) {
+            console.log(rsp);
+            if (rsp.success) {
+                var msg = '결제가 완료되었습니다.';
+                msg += '고유ID : ' + rsp.imp_uid;
+                msg += '상점 거래ID : ' + rsp.merchant_uid;
+                msg += '결제 금액 : ' + rsp.paid_amount;
+                msg += '카드 승인번호 : ' + rsp.apply_num;
+            } else {
+                var msg = '결제에 실패하였습니다.';
+                msg += '에러내용 : ' + rsp.error_msg;
+            }
+            alert(msg);
+        });
 	};
 	return{init:init, kakao:kakao, port:port};
 })();
