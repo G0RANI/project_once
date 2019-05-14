dc6d2c572deb46110f628cb1dd496bb0f64b20f8
gorani4545 <gorani4545@gmail.com>
2019-05-14 09:45:50 +0900

    9:45

--- src/main/java/com/once/web/controller/CommonController.java
+++ src/main/java/com/once/web/controller/CommonController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -65,6 +66,7 @@ public class CommonController {
 		return "payment";
 	}
 	
+	@Transactional
 	@SuppressWarnings("unchecked")
 	@ResponseBody
 	@RequestMapping(value ="/payment/{id}", method = RequestMethod.POST)
